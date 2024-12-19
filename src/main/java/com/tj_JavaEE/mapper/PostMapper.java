d773d8531467d83135e5a56a1af531ba479955e2
CChris152 <1650306859@qq.com>
2024-12-20 01:23:31 +0800

    add 帖子审核显示功能

--- src/main/java/com/tj_JavaEE/mapper/PostMapper.java
+++ src/main/java/com/tj_JavaEE/mapper/PostMapper.java
@@ -1,7 +1,13 @@
 package com.tj_JavaEE.mapper;
 
+import com.tj_JavaEE.dto.AuditPostInfo;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface PostMapper {
+    @Select("select p.post_id as postId, p.post_title as postTitle, c.category_name as categoryName, p.publish_time as publishTime, p.post_content as postContent, p.author_id as authorId, u.nickname as authorName from post p inner join category c on p.category_id = c.category_id inner join user u on p.author_id = u.user_id where p.status = 'pending'")
+    List<AuditPostInfo> auditPostInfoList();
 }
--- src/main/java/com/tj_JavaEE/service/impl/PostServiceimpl.java
+++ src/main/java/com/tj_JavaEE/service/impl/PostServiceimpl.java
@@ -1,12 +1,20 @@
 package com.tj_JavaEE.service.impl;
 
+import com.tj_JavaEE.dto.AuditPostInfo;
 import com.tj_JavaEE.mapper.PostMapper;
 import com.tj_JavaEE.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PostServiceimpl implements PostService {
     @Autowired
     private PostMapper postMapper;
+
+    @Override
+    public List<AuditPostInfo> auditPostInfoList() {
+        return postMapper.auditPostInfoList();
+    }
 }
